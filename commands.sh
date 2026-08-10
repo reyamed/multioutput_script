0fe38918e6e50f0368a8068e9ccb52f9e272c525
Ahmed AZIZ <ahmed.aziz@ump.ac.ma>
2026-08-10 09:45:22 -0500

    wip

--- commands.sh
+++ commands.sh
@@ -15,3 +15,16 @@ done | sort -rn | head -20
 echo "segments: $(find . -name '*.log' | wc -l)"
 echo "mappings needed: $(( $(find . -name '*.index' -o -name '*.timeindex' | wc -l) ))"
 sysctl -n vm.max_map_count
+
+
+# segment / partition
+cd /path/to/broker
+
+t=<the-topic>
+
+parts=$(find . -maxdepth 1 -type d -name "${t}-[0-9]*" | wc -l)
+idx=$(find . -path "./${t}-*" -name '*.index' | wc -l)
+
+echo "partitions:            $parts"
+echo "index files:           $idx"
+echo "segments per partition: $((idx / parts))"
